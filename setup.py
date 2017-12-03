cc54e2896972741ef72449d9f2ab9f4628e42eae
sergeymazin <sergey.mazin@pipedrive.com>
2017-12-03 15:38:58 +0200

    update version to 0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name='dynamodb_meta_store',
   packages=['dynamodb_meta_store'],
-  version='0.1',
+  version='0.2',
   description='Store metadata using DynamoDB as backend',
   author='Sergey Mazin',
   author_email='sergey.mazin@hotmail.com',
   url='https://github.com/sergeymazin/dynamodb-meta-store',
-  download_url='https://github.com/sergeymazin/dynamodb-meta-store/archive/0.1.tar.gz',
+  download_url='https://github.com/sergeymazin/dynamodb-meta-store/archive/0.2.tar.gz',
   keywords=['dynamodb', 'aws', 'config', 'metadata'],
   classifiers=[],
 )
